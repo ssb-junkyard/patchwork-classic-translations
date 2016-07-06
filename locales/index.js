78bc48d497aacf283ecdd087612184ecd85c3bbc
Tiago <tiago.c.pontes.p@gmail.com>
2016-07-06 23:32:59 +0100

    pt-PT

--- locales/index.js
+++ locales/index.js
@@ -1,6 +1,8 @@
 require('moment/locale/pt-br')
+require('moment/locale/pt')
 
 module.exports = {
   'en': require('./en'),
-  'pt-BR': require('./pt-BR')
+  'pt-BR': require('./pt-BR'),
+  'pt-PT': require('./pt-PT')
 }
